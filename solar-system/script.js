4b8b3e50c5d7085267e930cb535961a96dd575c6
porto <pedrolportow@gmail.com>
2025-07-17 21:35:29 -0300

    inicio do trabalho, planeta girando na volta do sol

--- solar-system/script.js
+++ solar-system/script.js
@@ -1,144 +1,108 @@
-"use strict";
+"use strict"
 
-var vs = `#version 300 es
-
-in vec4 a_position;
-in vec4 a_color;
-
-uniform mat4 u_matrix;
-
-out vec4 v_color;
-
-void main() {
-  // Multiply the position by the matrix.
-  gl_Position = u_matrix * a_position;
-
-  // Pass the color to the fragment shader.
-  v_color = a_color;
-}
-`;
-
-var fs = `#version 300 es
-precision highp float;
-
-// Passed in from the vertex shader.
-in vec4 v_color;
-
-uniform vec4 u_colorMult;
-
-out vec4 outColor;
-
-void main() {
-   outColor = v_color * u_colorMult;
-}
-`;
+import { vertexShader, fragmentShader } from "./shaders.js"
+import { degToRad } from "./utils.js"
 
 function main() {
-  var canvas = document.querySelector("#canvas");
-  var gl = canvas.getContext("webgl2");
+  const canvas = document.querySelector("#canvas")
+  const gl = canvas.getContext("webgl2")
 
-  // Tell the twgl to match position with a_position, n
-  // normal with a_normal etc..
-  twgl.setAttributePrefix("a_");
+  twgl.setAttributePrefix("a_") // prefixo para os atributos
 
-  var sphereBufferInfo = flattenedPrimitives.createSphereBufferInfo(gl, 10, 12, 6);
+  /**
+  * @param {WebGL2RenderingContext} gl
+  * @param {number} radius - raio
+  * @param {number} subdivisionAxis - numero de subdivisões no eixo x
+  * @param {number} subdivisionsHeight - numero de subdivisões no eixo y
+  * @returns {Object}
+  * @see https://twgljs.org/docs/module-twgl_primitives.html#.createSphereBufferInfo
+  */
+  const sunBuffer = flattenedPrimitives.createSphereBufferInfo(gl, 15, 12, 6)  // Sol r = 15, 12 segmentos, 6 subdivisões
+  const planetBuffer = flattenedPrimitives.createSphereBufferInfo(gl, 8, 12, 6)  // Planeta r = 8, 12 segmentos, 6 subdivisões
 
-  // setup GLSL program
-  var programInfo = twgl.createProgramInfo(gl, [vs, fs]);
+  const programInfo = twgl.createProgramInfo(gl, [vertexShader, fragmentShader])
 
-  var sphereVAO = twgl.createVAOFromBufferInfo(gl, programInfo, sphereBufferInfo);
+  const sunVAO = twgl.createVAOFromBufferInfo(gl, programInfo, sunBuffer)
+  const planetVAO = twgl.createVAOFromBufferInfo(gl, programInfo, planetBuffer)
 
-  function degToRad(d) {
-    return d * Math.PI / 180;
-  }
+  const sunUniforms = { u_colorMult: [1, 0.8, 0.2, 1], u_matrix: m4.identity() }
+  const planetUniforms = { u_colorMult: [0.2, 0.5, 1, 1], u_matrix: m4.identity() }
 
-  var fieldOfViewRadians = degToRad(60);
-
-  // Uniforms for each object.
-  var sphereUniforms = {
-    u_colorMult: [0.5, 1, 0.5, 1],
-    u_matrix: m4.identity(),
-  };
-  
-  var sphereTranslation = [  0, 0, 0];
-
-  var objectsToDraw = [
+  const objectsToDraw = [
     {
       programInfo: programInfo,
-      bufferInfo: sphereBufferInfo,
-      vertexArray: sphereVAO,
-      uniforms: sphereUniforms,
+      bufferInfo: sunBuffer,
+      vertexArray: sunVAO,
+      uniforms: sunUniforms,
     },
-  ];
+    {
+      programInfo: programInfo,
+      bufferInfo: planetBuffer,
+      vertexArray: planetVAO,
+      uniforms: planetUniforms,
+    },
+  ]
 
-  function computeMatrix(viewProjectionMatrix, translation, xRotation, yRotation) {
-    var matrix = m4.translate(viewProjectionMatrix,
-        translation[0],
-        translation[1],
-        translation[2]);
-    matrix = m4.xRotate(matrix, xRotation);
-    return m4.yRotate(matrix, yRotation);
-  }
-
-  requestAnimationFrame(drawScene);
-
-  // Draw the scene.
-  function drawScene(time) {
-    time = time * 0.0005;
-
-    twgl.resizeCanvasToDisplaySize(gl.canvas);
-
-    // Tell WebGL how to convert from clip space to pixels
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-    gl.enable(gl.CULL_FACE);
-    gl.enable(gl.DEPTH_TEST);
-
-    // Compute the projection matrix
-    var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    var projectionMatrix =
-        m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
-
-    // Compute the camera's matrix using look at.
-    var cameraPosition = [0, 0, 100];
-    var target = [0, 0, 0];
-    var up = [0, 1, 0];
-    var cameraMatrix = m4.lookAt(cameraPosition, target, up);
-
-    // Make a view matrix from the camera matrix.
-    var viewMatrix = m4.inverse(cameraMatrix);
-
-    var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
-
-    var sphereXRotation =  time;
-    var sphereYRotation =  time;
-
-    // Compute the matrices for each object.
-    sphereUniforms.u_matrix = computeMatrix(
-        viewProjectionMatrix,
-        sphereTranslation,
-        sphereXRotation,
-        sphereYRotation);
-
-
-    // ------ Draw the objects --------
-
-    objectsToDraw.forEach(function(object) {
-      var programInfo = object.programInfo;
-
-      gl.useProgram(programInfo.program);
-
-      // Setup all the needed attributes.
-      gl.bindVertexArray(object.vertexArray);
-
-      // Set the uniforms we just computed
-      twgl.setUniforms(programInfo, object.uniforms);
-
-      twgl.drawBufferInfo(gl, object.bufferInfo);
-    });
-
-    requestAnimationFrame(drawScene);
-  }
+  requestAnimationFrame((time) => drawScene({ time, gl, fieldOfViewRadians: degToRad(60), objectsToDraw, sunUniforms, planetUniforms }))
 }
 
-main();
+// calcula a matriz de transformação final 
+// todo: mudar pra primeiro calcular todas as transformações e depois aplicar na viewProjectionMatrix?
+function computeMatrix(viewProjectionMatrix, translation, xRotation, yRotation) {
+  // transalação + rotação x + rotação y
+  let matrix = m4.translate(viewProjectionMatrix, translation[0],  translation[1],  translation[2]) 
+
+  matrix = m4.xRotate(matrix, xRotation)
+  return m4.yRotate(matrix, yRotation)
+}
+
+function drawScene({ time, gl, fieldOfViewRadians, objectsToDraw, sunUniforms, planetUniforms }) {
+  time = time * 0.0005
+
+  twgl.resizeCanvasToDisplaySize(gl.canvas)
+
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+
+  gl.enable(gl.CULL_FACE)
+  gl.enable(gl.DEPTH_TEST)
+
+  gl.clearColor(0, 0, 0, 1)
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+  const aspectRatio = gl.canvas.clientWidth / gl.canvas.clientHeight
+  const projectionMatrix = m4.perspective(fieldOfViewRadians, aspectRatio, 1, 2000)
+
+  // buildar a camera
+  const cameraPosition = [0, 0, 120]
+  const lookVector = [0, 0, 0]
+  const upVector = [0, 1, 0]
+  const cameraMatrix = m4.lookAt(cameraPosition, lookVector, upVector)
+
+  const viewMatrix = m4.inverse(cameraMatrix)
+  const viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix)
+
+  const sunTranslation = [0, 0, 0]
+  sunUniforms.u_matrix = computeMatrix(viewProjectionMatrix, sunTranslation, 0, 0)
+
+  // Planeta orbitando
+  const planetOrbitRadius = 50
+  const planetOrbitSpeed = time * 1.5
+
+  const planetTranslation = [ Math.cos(planetOrbitSpeed) * planetOrbitRadius, 0, Math.sin(planetOrbitSpeed) * planetOrbitRadius]
+  const planetRotation = time * 2
+
+  planetUniforms.u_matrix = computeMatrix( viewProjectionMatrix, planetTranslation, 0, planetRotation)
+
+  objectsToDraw.forEach(function(object) {
+    const programInfo = object.programInfo
+
+    gl.useProgram(programInfo.program)
+    gl.bindVertexArray(object.vertexArray)
+    twgl.setUniforms(programInfo, object.uniforms)
+    twgl.drawBufferInfo(gl, object.bufferInfo)
+  })
+
+  requestAnimationFrame((time) => drawScene({ time, gl, fieldOfViewRadians, objectsToDraw, sunUniforms, planetUniforms }))
+}
+
+main()
---
+++ solar-system/utils.js
@@ -0,0 +1,3 @@
+export function degToRad(d) {
+  return d * Math.PI / 180;
+}
