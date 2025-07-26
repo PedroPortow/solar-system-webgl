790f371916281d44419d8632cbc3f544d3c87bc2
porto <pedrolportow@gmail.com>
2025-07-26 17:38:20 -0300

    trajetoria dos planetas

--- solar-system/utils/main.js
+++ solar-system/utils/main.js
@@ -1,3 +1,17 @@
 export function degToRad(d) {
   return d * Math.PI / 180;
 }
+
+
+// créditos: chatgpt 🤖
+export function convertHGIToCartesian(radAU, latDeg, lonDeg, AU_TO_UNITS = 50) {
+  const radius = radAU * AU_TO_UNITS
+  const latRad = degToRad(latDeg)
+  const lonRad = degToRad(lonDeg)
+  
+  const x = radius * Math.cos(latRad) * Math.cos(lonRad)
+  const y = radius * Math.sin(latRad)
+  const z = radius * Math.cos(latRad) * Math.sin(lonRad)
+  
+  return [x, y, z]
+}
